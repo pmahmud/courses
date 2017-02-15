07718e009f21a779dbb8c7789b843b7a8be641f8
Pallab Mahmud <pmahmud@commercehub.com>
2017-02-14 16:54:28 -0800

    Mongoose and OAuthPassportJS

---
+++ source/isomorphic/Readme.md
@@ -0,0 +1,2 @@
+https://github.com/bananaoomarang/isomorphic-redux.git
+https://medium.com/front-end-developers/handcrafting-an-isomorphic-redux-application-with-love-40ada4468af4#.kcru9gmzl
---
+++ source/mongoose/models/standup.server.model.js
@@ -0,0 +1,17 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var standupSchema = new Schema({
+    memberName: String,
+    project: String,
+    workYesterday: String,
+    workToday: String,
+    impediment: String,
+    createdOn: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Standup', standupSchema);
+/*
+    Schema has an add method
+    standupSchema.add({blah: foo});
+ */
